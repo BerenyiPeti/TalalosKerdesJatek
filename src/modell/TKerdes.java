cb22bf5b37e55fa8fc915dcd434a1012b560321b
BerenyiPeti <berenyipeti01@gmail.com>
2022-05-31 08:39:27 +0200

    alap mappaszerkezet

---
+++ src/modell/TKerdes.java
@@ -0,0 +1,13 @@
+package modell;
+
+public class TKerdes {
+    private String kerdes;
+    private int valasz;
+
+    public TKerdes(String kerdes, int valasz) {
+        this.kerdes = kerdes;
+        this.valasz = valasz;
+    }
+    
+    
+}
