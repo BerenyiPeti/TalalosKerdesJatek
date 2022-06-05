706ff331dec90b947fa82785ffccdd00432d1877
BerenyiPeti <berenyipeti01@gmail.com>
2022-06-05 23:16:08 +0200

    beker változó eltávolítva a mainből

--- src/main/Program.java
+++ src/main/Program.java
@@ -4,8 +4,6 @@ import nezet.TKerdesGui;
 
 public class Program {
 
-    private static int beker;
-
     public static void main(String[] args) {
         TKerdesGui gui = new TKerdesGui();
         gui.main(args);
